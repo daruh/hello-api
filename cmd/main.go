b692d30461f3dec12bc4f531758b887185b2e0fe
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-04-03 10:59:36 +0200

    configmap startup

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"gopkg.in/yaml.v2"
 	"hello-api/handlers/rest"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+const CONFIG_FILE = "/etc/config/configmap-microservice-demo.yaml"
+
+type Config struct {
+	Message string `yaml:"message"`
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+}
+
+func loadConfig(configFile string) *Config {
+	conf := &Config{}
+	configData, err := ioutil.ReadFile(configFile)
+	check(err)
+
+	err = yaml.Unmarshal(configData, conf)
+	check(err)
+	log.Println(conf)
+	return conf
+}
 
 func main() {
 
@@ -16,6 +41,7 @@ func main() {
 
 	log.Printf("listening on %s\n", addr)
 	log.Println("config map demo")
+	loadConfig(CONFIG_FILE)
 
 	log.Fatal(http.ListenAndServe(addr, mux))
 }
